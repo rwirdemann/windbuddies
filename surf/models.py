d3e631d61726e9384609f60f942c6aa2034f23f8
Ralf Wirdemann <ralf.wirdemann@codekeepers.de>
2021-03-06 11:48:45 +0100

    Adds spot images

--- surf/models.py
+++ surf/models.py
@@ -6,5 +6,8 @@ class Session(models.Model):
     spot = models.CharField(max_length=100)
     planned = models.IntegerField(default=0)
 
+    def spot_image(self):
+        return '/static/surf/' + self.spot.replace(' ', '').lower() + '.png'
+
     def __str__(self):
         return '%s: %s Riders: %d' % (self.when.strftime("%d.%m.%Y"), self.spot, self.planned)
