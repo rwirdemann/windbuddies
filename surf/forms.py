f521aa90072d8f8d5f6a7133035f955085e76515
Ralf Wirdemann <ralf.wirdemann@codekeepers.de>
2021-03-17 16:00:17 +0100

    Adapts new session form to django standard

--- surf/forms.py
+++ surf/forms.py
@@ -1,5 +1,22 @@
+from django.forms import ModelForm
+from .models import Session
 from django import forms
+import django.utils.timezone
 
-class SessionForm(forms.Form):
-    spot = forms.CharField(label='Wo', max_length=100)
-    when = forms.DateField(label='Wann')
+
+class SessionForm(ModelForm):
+    spot = forms.CharField(
+        max_length=50,
+        label='Wo',
+        widget=forms.TextInput(attrs={
+            'autofocus': True,
+            'placeholder': 'Name des Spots',
+            'class': 'form-control'
+        }))
+    when = forms.CharField(widget=forms.SelectDateWidget(),
+                           label='Wann',
+                           initial=django.utils.timezone.now())
+
+    class Meta:
+        model = Session
+        fields = ['spot', 'when']
--- surf/views.py
+++ surf/views.py
@@ -1,25 +1,30 @@
 from django.http.response import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 import requests
+import datetime
 
 from .models import Session
 from .forms import SessionForm
 
 
 def index(request):
-    sessions = Session.objects.order_by('-when')[:5]
-
+    sessions = Session.objects.order_by('-when')[:8]
     context = {'latest_sessions': sessions}
     return render(request, 'surf/index.html', context)
 
 
 def create_session(request):
-    form = SessionForm(request.POST)
-    s = Session(spot=form.data['spot'], when=form.data['when'])
-    s.save()
-    return HttpResponseRedirect('/surf')
+    if request.method == 'POST':
+        f = SessionForm(request.POST)
+        f.save()
+        return HttpResponseRedirect('/surf')
+    else:
+        form = SessionForm()
+
+    return render(request, 'surf/session.html', {'form': form})
 
 
 def actual_weather(request):
-    json = requests.get('https://9nehnu4h6h.execute-api.us-east-1.amazonaws.com/stage/').text
+    json = requests.get(
+        'https://9nehnu4h6h.execute-api.us-east-1.amazonaws.com/stage/').text
     return HttpResponse(json, content_type='application/json')
