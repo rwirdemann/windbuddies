350b5f40b59d1437295cbcc88fa0087b2c2868c5
Ralf Wirdemann <ralf.wirdemann@codekeepers.de>
2021-03-18 15:17:36 +0100

    Adds riders

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Session
+from .models import Session, Rider
 
 admin.site.register(Session)
+admin.site.register(Rider)
--- main/models.py
+++ main/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 import django.utils.timezone
 
+
+class Rider(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Session(models.Model):
     when = models.DateField('when', default=django.utils.timezone.now)
     spot = models.CharField(max_length=100)
     planned = models.IntegerField(default=0)
+    riders = models.ManyToManyField(Rider)
 
     def spot_image(self):
         return '/static/main/' + self.spot.replace(' ', '').lower() + '.png'
 
     def __str__(self):
-        return '%s: %s Riders: %d' % (self.when.strftime("%d.%m.%Y"), self.spot, self.planned)
+        return '%s: %s Riders: %d' % (self.when.strftime("%d.%m.%Y"),
+                                      self.spot, self.riders.count())
